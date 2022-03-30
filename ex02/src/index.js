7a162c326fd81866ecb0a3649ff5a01340dfc647
Sumedin Bilalic <sumko87@gmail.com>
2022-03-30 21:30:23 +0200

    just practicing comments in JS

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //Only change code bellow this line
+    //This i my first single line comment in JS
+    /*This is a way
+    to write multi-line comment in JS
+    */
+    //Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
